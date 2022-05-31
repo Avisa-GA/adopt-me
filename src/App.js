7c44765e45244673870e1456468839a416fb057a
Avisa Poshtkouhi <aap571420@gmail.com>
2022-05-31 15:20:52 -0700

    feat: component module

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import { render } from "react-dom";
+import Pet from "./Pet.js";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
+    <Pet name="Luna" animal="Dog" breed="Havanese" />,
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />,
+    <Pet name="Dojnk" animal="Cat" breed="Mix" />,
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
