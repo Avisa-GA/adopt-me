30da31fe828b8fc5ee913c96825210c60ffa9355
Avisa Poshtkouhi <aap571420@gmail.com>
2022-06-06 15:07:04 -0700

    feat: custom hook

--- src/Pet.js
+++ src/Pet.js
@@ -8,12 +8,4 @@ const Pet = (props) => {
   );
 };
 
-// const Pet = ({ name, animal, breed }) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, name),
-//     React.createElement("h2", {}, animal),
-//     React.createElement("h2", {}, breed),
-//   ]);
-// };
-
 export default Pet;
