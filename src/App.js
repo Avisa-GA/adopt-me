f38d6ddc25306d6778148a249f1e2c1e484fc761
Avisa Poshtkouhi <aap571420@gmail.com>
2022-06-08 14:42:42 -0700

    feat: React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams.js";
+import { StrictMode } from "react";
+import Details from "./Details";
 
+// replace App
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route exact path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
